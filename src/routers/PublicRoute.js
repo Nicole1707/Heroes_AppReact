07f3abd351e69acf10afce9adc7545f41f6c810f
Nicole <67175550+Nicole1707@users.noreply.github.com>
2021-11-26 08:50:30 -0800

    Initial Commit

---
+++ src/routers/PublicRoute.js
@@ -0,0 +1,14 @@
+import { useContext } from "react"
+import { Navigate } from 'react-router'
+import { AuthContext } from "../components/auth/authContext"
+
+
+
+export const PublicRoute = ({ children }) => {
+    const { user } = useContext(AuthContext)
+    return user.logged
+        ? <Navigate to="/marvel" />
+        : children
+
+}
+
